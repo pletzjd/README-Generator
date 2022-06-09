b57f1e956b36dc5a8581077e4c4e143ea642b747
pletzjd <j.pletzer17@gmail.com>
2022-06-08 20:41:32 -0400

    Created questions array.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let inquirer = require('inquirer');
 let fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project's title?","Please type in a description of your project: ","Please type out installation instructions: ","Please type out usage information: ","Please type out contribution guidlines: ","Please type out test instructions: ", "What license would you like to use for your project? (GNU AGPLv3, GNU GPLv3, GNU LGPLv3, Mozilla Public License 2.0, Apache License 2.0, MIT License, Boost Software License 1.0, The Unlicense) ","What is your GitHub username? ","What is your email address? "];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

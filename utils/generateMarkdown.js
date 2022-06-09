32fdfdf80da9fb8f380df146b361ae5d5d1063f8
pletzjd <j.pletzer17@gmail.com>
2022-06-09 01:23:59 -0400

    README is mostly generated. Need to add a license based on user input and implimented a badge for the license.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,54 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#test)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  
+  ---
+  
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Contributing
+  
+  ${data.constribution}
+  
+  ## Test
+  
+  ${data.test}
+  
+  ## Questions
+  
+  My GitHub profile can be found here: [https://github.com/${data.username}](https://github.com/${data.username})<br>
+  Or I can be reached by email at: ${data.email}
+
 `;
 }
 

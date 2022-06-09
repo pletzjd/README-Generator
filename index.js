2e8d8d44a2693d9bb52488fd599360d7e6279bb4
pletzjd <j.pletzer17@gmail.com>
2022-06-08 20:24:44 -0400

    Imported inquirer and fs libraries to index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+let inquirer = require('inquirer');
+let fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
